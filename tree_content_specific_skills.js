44f56f458ad5b203fdeb444b82c00f94779f90c6
Alvin Jeffery <alvinjeffery@gmail.com>
2019-07-26 07:29:13 -0500

    added v02 with tabs, starting to parse tree content

---
+++ tree_content_specific_skills.js
@@ -0,0 +1,75 @@
+  var treeData =
+    {
+     "name": "Start",
+       "children": [
+        { // 2nd level
+         "name": "Data Collection",
+		 "comment": "Learn how to collect/retrieve data from databases",
+         "children": [
+          {"name": "Coursera - all SQL courses", "comment": "Multiple courses for learning SQL", "url": "https://www.coursera.org/courses?query=sql&indices%5Bprod_all_products_custom_ranking_revenuelast28d%5D%5BrefinementList%5D%5Bskills%5D%5B0%5D=Sql&indices%5Bprod_all_products_custom_ranking_revenuelast28d%5D%5Bpage%5D=1&indices%5Bprod_all_products_custom_ranking_revenuelast28d%5D%5Bconfigure%5D%5BclickAnalytics%5D=true&indices%5Bprod_all_products_custom_ranking_revenuelast28d%5D%5Bconfigure%5D%5BhitsPerPage%5D=10&configure%5BclickAnalytics%5D=true"},
+          {"name": "Coursera - SQL for Data Science", "comment": "Popular SQL course. ~20 hours to complete. Can audit for free", "url": "https://www.coursera.org/learn/sql-for-data-science"},
+		  {"name": "DataCamp - SQL Fundamentals", "comment": "4-course track to learn SQL. Paid subscription required.", "url": "https://www.datacamp.com/tracks/sql-fundamentals"},
+		  {"name": "W3Schools", "comment": "Free, self-paced tutorials with practice examples", "url": "https://www.w3schools.com/sql/default.asp"},
+		  {"name": "Software Carpentry", "comment": "Free, self-paced tutorial. Can be delivered as in-person workshop.", "url": "http://swcarpentry.github.io/sql-novice-survey/"},
+         ]
+        },
+        { // 2nd level
+         "name": "Data Analysis",
+		 "comment": "Learn how to anlyze data, develop models, make predictions, & visualize results",
+         "children": [
+			 { // 3rd level
+				 "name": "Online - Coursera",
+			  	 "comment": "An online learning environment with courses in many fields (not just data science) - can audit all courses for free", 
+			  	 "children": [ 
+				  {"name": "Data Science Specialization (Johns Hopkins University)", "comment": "10-course (~8 months) comprehensive curriculum (R-based)", "url": "https://www.coursera.org/specializations/jhu-data-science"},
+				  {"name": "Data Science Specialization (University of Michigan)", "comment": "5-course (~5 months) introductory curriculum (Python-based)", "url": "https://www.coursera.org/specializations/data-science-python"},
+				  {"name": "Popular Data Science Courses", "comment": "2018's most popular courses, according to Coursera", "url": "https://www.coursera.org/collections/popular-data-science-courses-2018"},
+			  ]
+		  	 },
+             { // 3rd level
+                 "name": "Online - Other", 
+				 "comment": "Free & paid online resources", 
+                 "children": [
+		   		     {"name": "DataCamp", "comment": "Covers all aspects of data science. Includes individual courses & entire tracks. Paid subscription required.", "url": "https://www.datacamp.com"},
+		   		  	 {"name": "DataQuest", "comment": "Can learn Python, R, SQL, & theory via interactive exercise. Paid subscription required.", "url": "https://www.dataquest.io"},
+		   		  	 {"name": "Introduction to Statistical Learning (Stanford)", "comment": "Free, self-paced course covering statistical & machine learning techniques. (Uses R)", "url": "https://online.stanford.edu/courses/sohs-ystatslearning-statistical-learning-self-paced"},
+					 {"name": "Metacademy", "comment": "Search for specific concepts (e.g., 'random forests') to find multiple learning activities", "url": "https://metacademy.org"},
+					 {"name": "Udemy - Python for Data Science and Machine Learning Bootcamp", "comment": "Comprehensive, applied curriculum for Python & SQL. Paid subscription required.", "url": "https://www.udemy.com/python-for-data-science-and-machine-learning-bootcamp"}, 
+					 {"name": "Other Options", "comment": "Blog description of popular online data science learning options", "url": "https://www.learndatasci.com/best-data-science-online-courses/"},
+		             
+				 ]
+             },
+             { // 3rd level
+                 "name": "In-Person Workshops", 
+				 "comment": "Immersive experiences in a physical location",
+                 "children": [
+					 {"name": "Hands-On Data Science Introduction (Minneapolis)", "comment": "Offered as a full-day pre-conference at the Nursing Knowledge: Big Data Science Conferece", "url": "https://www.nursing.umn.edu/centers/center-nursing-informatics/news-events/2019-nursing-knowledge-big-data-science-conference"},
+                     {"name": "Data Visualization & Analytics Bootcamp (Minneapolis)", "comment": "24-week, comprehensive data science workshop (not healthcare specific)", "url": "https://bootcamp.umn.edu/data/"},
+                 ]
+             } 
+		 ]
+	    },
+		{ // 2nd level
+         "name": "Software",
+		 "comment": "Commonly used software applications for data science",
+         "children": [
+          {"name": "Python Software", "comment": "Data Science application of choice for most data scientists. (Free, open-source)", "url": "https://www.python.org"},
+		  {"name": "R Software", "comment": "2nd most common data science software. Used by many statisticians. (Free, open-source)", "url": "https://www.r-project.org"},
+		  {"name": "Software Carpentry", "comment": "Free, self-paced tutorials for common software applications (e.g., Python, R, SQL, Git, command line)", "url": "https://software-carpentry.org"},
+         ]
+	    },
+        { // 2nd level
+         "name": "Leading & Working on Teams",
+	     "comment": "Contribute to data science projects, regardless of your theoretical or programming knowledge",
+         "children": [
+         	{"name": "Coursera - Executive Data Science Specialization", "comment": "4-courses related to helping you effectively lead data science teams - can audit individual courses for free", "url": "https://www.coursera.org/specializations/executive-data-science"},
+	  	  	{"name": "How to Structure a Data Science Team", "comment": "Considerations for models & roles of data science teams", "url": "https://www.altexsoft.com/blog/datascience/how-to-structure-data-science-team-key-models-and-roles/"},
+			{"name": "Data Science for Healthcare", "comment": "Healthcare-focused review of the role of data science", "url": "https://www.healthcatalyst.com/data-science-healthcare-what-leaders-must-know"},
+        ]
+       },
+	   { // 2nd level
+		 "name": "How to Learn Data Science (YouTube)", "comment": "30-minute overview of general approach to learning data science (sponsored by DataQuest)", "url": "https://www.youtube.com/watch?v=Ura_ioOcpQI"
+	   }, 
+       ]
+    }
+
